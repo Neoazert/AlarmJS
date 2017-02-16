f3577982604d0ffeae62e1653e43e3d01a9f6338
Quincy <quincywww@gmail.com>
2017-02-16 11:51:50 +0100

    undefined

--- script.js
+++ script.js
@@ -18,53 +18,101 @@ function set_time(){
 }
 
 
-setInterval(
-    function () {
-        get_time();
-        document.getElementById('time').textContent=datetime;
+function delete_alarm(elemToDelt){
+    elemToDelt.remove();
+
+}
+
+
+function add_alarm(){
+            var alamrmElem = document.createElement("div");
+
+            var cb = document.createElement('input');
+            cb.type = 'checkbox';
+            cb.className = 'active';
+            cb.checked = false;
+            var num = document.createElement('input');
+            num.type = 'number';
+            num.className = 'h';
+            var num2 = document.createElement('input');
+            num2.type = 'number';
+            var txt = document.createElement('input');
+            txt.type = 'text';
+            var sel = document.createElement('select');
+            var but = document.createElement('button');
+
+            but.textContent = "delete";
+
+            but.addEventListener("click", function() {delete_alarm(alamrmElem);});
+
+
+
+            alamrmElem.appendChild(cb);
+            alamrmElem.appendChild(num);
+            alamrmElem.appendChild(num2);
+            alamrmElem.appendChild(txt);
+            alamrmElem.appendChild(sel);
+            alamrmElem.appendChild(but);
+
+
+            document.getElementById('alarmDiv').appendChild(alamrmElem);
+ }
+
+
+function parcourElem() {
+
+    var ancestor = document.getElementById('alarmDiv');
+    var descendents = ancestor.getElementsByTagName('*');
+    var e;
+    //console.log(descendents);
+
+    for (i = 0; i < descendents.length; ++i) {
+
+        e = descendents[i];
+        //console.log(e);
+        console.log("***********************************");
+        //console.log(e.getElementsByClassName('active'));
+        
+        //console.log(e.getElementsByClassName('active').checked);
+        
+        console.log(e.getElementsByClassName('h').value);
+        console.log(e.getElementsByClassName('h'));
+        
+        
+        if (e.getElementsByClassName('active').checked){
+            console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        }
+
+        i++;
 
     }
-    ,1000);
 
+
+
+
+}
 
 
 // ******** PAGE LOAD *******
 
 function load_page() {
 
-    set_time()
+    set_time();
 
-    // Button add alarm
-    document.getElementById('addAlarmButton').addEventListener("click",
-        function() {
-            console.log("click");
-            var alamrmElem = document.createElement("div");
-            
-            var cb = document.createElement('input');
-            cb.type = 'checkbox';
-            var num = document.createElement('input');
-            num.type = 'number';
-            var txt = document.createElement('input');
-            txt.type = 'text';
-            var sel = document.createElement('select');
-            var but = document.createElement('button');
-            but.textContent = "-";
-            
-            
-            alamrmElem.appendChild(cb);
-            alamrmElem.appendChild(num);
-            alamrmElem.appendChild(num);
-            alamrmElem.appendChild(txt);
-            alamrmElem.appendChild(sel);
-            alamrmElem.appendChild(but);
-            
-                    
-                    
-            document.getElementById('alarmDiv').appendChild(alamrmElem);
-            }
-          );
+    document.getElementById('addAlarmButton').addEventListener("click",add_alarm);
 
 
 }
-
 window.addEventListener("load", load_page);
+
+
+// ******** REFRESH *******
+
+setInterval(
+    function () {
+        set_time();
+
+        parcourElem();
+
+    }
+    ,1000);
